b9c61e0f9d8ecbafc78905a5e4c86e58225d2ace
TomMonks <t.m.w.monks@exeter.ac.uk>
2026-01-06 11:21:44 +0000

    feat(pkg): updated exports from init

--- json2ciw/__init__.py
+++ json2ciw/__init__.py
@@ -1,11 +1,12 @@
 __author__ = "Tom Monks"
 __version__ = "0.1.0"
 
-from .datasets import load_call_centre_model
+from .datasets import load_call_centre_model, load_model_file
 from .engine import create_ciw_network
 
 # define what is exported
 __all__ = [
+    "load_model_file"
     "load_call_centre_model",
     "create_ciw_network",
     "__version__",
