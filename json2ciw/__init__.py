d13079b65e8d9841d1cda69d52ef0fbccabe37f8
TomMonks <thomas.m.monks@gmail.com>
2026-01-14 11:46:46 +0000

    feat(pkg): bump version

--- json2ciw/__init__.py
+++ json2ciw/__init__.py
@@ -1,14 +1,17 @@
 __author__ = "Tom Monks"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from .datasets import load_call_centre_model, load_model_file
-from .engine import CiwConverter
+from .engine import CiwConverter, multiple_replications
+from .results import summarise_results
 
 # define what is exported
 __all__ = [
     "load_model_file"
     "load_call_centre_model",
     "CiwConverter",
+    "multiple_replications"
+    "summarise_results"
     "__version__",
     "__author__"
 ]
